bed85fcbbe7f2facbf2ace1b51e9f2114d2966ed
Max DELIN <you@example.com>
2017-12-06 22:29:36 +0100

    add cast service in movie.component.ts

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../../services/movie.service'
+import { MovieService } from '../../services/movie.service';
+import { CastService } from '../../services/cast.service';
 import { ActivatedRoute } from "@angular/router";
 import { Movie } from '../../models/movie';
+import { Cast } from '../../models/Cast';
 
 @Component({
   selector: 'app-movie',
@@ -11,9 +13,11 @@ import { Movie } from '../../models/movie';
 export class MovieComponent implements OnInit {
   id: number;
   movie: Movie;
+  cast: Cast;
 
   constructor(
     private movieService: MovieService,
+    private castService: CastService,
     private route: ActivatedRoute,
   ) { }
 
@@ -21,6 +25,7 @@ export class MovieComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.id = +params['id']; 
       this.getMovie(this.id);
+      this.getCast(this.id);
    });
   }
 
@@ -31,6 +36,15 @@ export class MovieComponent implements OnInit {
         const arr = movies.filter(movie => movie.id === id);
         this.movie = arr[0];
       });
+  }
+
+  getCast(id: number): void {
+    this.castService
+        .getCast()
+        .subscribe(casts => {
+          const arr = casts.filter(cast => cast.id === id);
+          this.cast = arr[0];
+        })
   }
 
 }
