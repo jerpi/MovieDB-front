fc2c750b854c07d2b75cdc92673bc0e897ae12e5
Max DELIN <you@example.com>
2017-12-12 18:33:30 +0100

    use components for movie details, show genres works

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -12,28 +12,21 @@ import { Cast } from '../../models/Cast';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
-  id: number;
+  
   movie: Movie;
   cast: Cast;
-  dangerousUrl: string;
-  dangerousVideoUrl: string;
-  trustedUrl: SafeUrl;
-  videoUrl: SafeResourceUrl;
 
   constructor(
     private movieService: MovieService,
     private castService: CastService,
     private route: ActivatedRoute,
-    private sanitizer: DomSanitizer,
-  ) { 
-    this.trustedUrl = sanitizer.bypassSecurityTrustUrl(this.dangerousUrl);
-  }
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.id = +params['id'];
-      this.getMovie(this.id);
-      this.getCast(this.id);
+      const id = +params['id'];
+      this.getMovie(id);
+      this.getCast(id);
       
    });
   }
@@ -44,7 +37,6 @@ export class MovieComponent implements OnInit {
       .subscribe(movies => {
         const arr = movies.filter(movie => movie.id === id);
         this.movie = arr[0];
-        this.updateVideoUrl(this.movie.bande_annonce);
       });
   }
 
@@ -55,12 +47,6 @@ export class MovieComponent implements OnInit {
           const arr = casts.filter(cast => cast.id === id);
           this.cast = arr[0];
         });
-  }
-
-  updateVideoUrl(key: string) {
-    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + key;
-    this.videoUrl =
-        this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
   }
 
   
